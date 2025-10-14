395bd552691a9e81f29958d66a86aefe3f74f12e
7ank0v1c <40895945+7ank0v1c@users.noreply.github.com>
2025-10-14 23:22:44 +0100

    Create log_utils.py

---
+++ N64SaveConverterGui/src/n64_converter/core/log_utils.py
@@ -0,0 +1,26 @@
+# log_utils.py
+from datetime import datetime
+
+# Terminal Colours
+class TermColors:
+    RESET = "\033[0m"
+    RED = "\033[31m"
+    GREEN = "\033[32m"
+    YELLOW = "\033[33m"
+    BLUE = "\033[34m"
+    MAGENTA = "\033[35m"
+    CYAN = "\033[36m"
+    WHITE = "\033[37m"
+
+# GUI logging
+def gui_log(log_box, message, level="info"):
+    tags = {
+        "info": "level_info",
+        "conversion": "level_conversion",
+        "warn": "level_warn",
+        "error": "level_error"
+    }
+    timestamp = datetime.now().strftime("[%H:%M:%S] ")
+    log_box.insert("end", timestamp, "timestamp")
+    log_box.insert("end", message + "\n", tags.get(level, "level_info"))
+    log_box.see("end")  # auto-scroll
